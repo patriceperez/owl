460fce28960bb9ea1b5d7cb024fc2eff6dfc2cfa
Patrice <perezpatrice@gmail.com>
2020-10-04 06:44:51 +0300

    initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const App: React.FC = () => {
+    return <h1>H ello Worl d!!!</h1>
+} 
---
+++ webpack.common.js
@@ -0,0 +1,50 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'development',
+  entry: './build/module/index.js',
+  devtool: 'inline-source-map',
+  target: 'electron-renderer',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              [
+                '@babel/preset-env',
+                {
+                  targets: {
+                    esmodules: true,
+                  },
+                },
+              ],
+              '@babel/preset-react',
+            ],
+          },
+        },
+      },
+      {
+        test: [/\.s[ac]ss$/i, /\.css$/i],
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js'],
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve('build', 'js'),
+  },
+}
