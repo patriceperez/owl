b7b836e8531be452c42c03dbb03ab5fca69ad12d
Patrice <perezpatrice@gmail.com>
2020-10-04 12:31:24 +0300

    moving App component to the correct folder

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import api from '../ipc/IpcService'
+import api from '../../ipc/IpcService'
 
 const notify = () => api.notify({ title: "Stom taipei", body: 'this is the owl notification body', subtitle: "what is this?"})
 
